315d88c88822de934ceaae00f5da0c51296184df
FelixHui118 <felix.hui@accenture.com>
2018-01-03 16:26:40 +0800

    add camera

--- App.js
+++ App.js
@@ -10,10 +10,20 @@ import {
   StyleSheet,
   Text,
   View,
-  ScrollView,
-  FlatList,
-  ToolbarAndroid
+  ToolbarAndroid,
+  Button,
+  Dimensions
 } from 'react-native';
+
+import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
+
+import Camera from 'react-native-camera';
+
+
+const initialLayout = {
+  height: 0,
+  width: Dimensions.get('window').width,
+};
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -22,31 +32,97 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const onBarCodeRead = (e) => {
+  console.log(
+      "Barcode Found!",
+      "Type: " + e.type + "\nData: " + e.data
+  );
+}
 
+const takePicture = () => {
+  const options = {};
+  //options.location = ...
+  console.log('a');
+  this.camera.capture({metadata: options})
+    .then((data) => console.log(data))
+    .catch(err => console.error(err));
+  console.log('Felix');
+}
+
+const onPressLearnMore = () => {}
+
+const FirstRoute = () => <View style={[ styles.container, { backgroundColor: '#ff4081' } ]} >
+  <Text style={styles.welcome}>
+    Welcome to React Native!Felixa
+  </Text>
+  <Text style={styles.instructions}>
+    To get started, edit App.js
+  </Text>
+  <Text style={styles.instructions}>
+    {instructions}
+  </Text>
+</View>;
+const SecondRoute = () => <View style={[ cameraStyle.container, { backgroundColor: '#673ab7' } ]}>
+  <Camera
+      ref={(cam) => {
+        this.camera = cam;
+      }}
+onBarCodeRead={onBarCodeRead.bind(this)}
+      style={cameraStyle.preview}
+      aspect={Camera.constants.Aspect.fill}>
+      <Text style={cameraStyle.capture} onPress={takePicture.bind(this)}>[CAPTURE]</Text>
+  </Camera>
+  <Button
+    onPress={onPressLearnMore}
+    title="Learn More"
+    color="#841584"
+    accessibilityLabel="Learn more about this purple button"
+  />
+</View>;
+
+const Second3Route = () => <View style={[ cameraStyle.container, { backgroundColor: '#673ab7' } ]}>
+  <Button
+    onPress={onPressLearnMore}
+    title="Learn More"
+    color="#841584"
+    accessibilityLabel="Learn more about this purple button"
+  />
+</View>;
 
 export default class App extends Component<{}> {
+
+  state = {
+    index: 0,
+    routes: [
+      { key: 'first', title: 'First' },
+      { key: 'second', title: 'Second' },
+      { key: 'first1', title: 'First' },
+      { key: 'second2', title: 'Second' }
+    ],
+  };
+
+  _handleIndexChange = index => this.setState({ index });
+
+  _renderHeader = props => <TabBar {...props} />;
+
+  _renderScene = SceneMap({
+    first: FirstRoute,
+    second: SecondRoute,
+    first1: FirstRoute,
+    second2: Second3Route
+  });
+
 
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView >
-          <Text style={styles.welcome}>
-            Welcome to React Native!Felix
-          </Text>
-          <Text style={styles.instructions}>
-            To get started, edit App.js
-          </Text>
-          <Text style={styles.instructions}>
-            {instructions}
-          </Text>
-        </ScrollView>
-        <FlatList
-          data={[{key: 'a'}, {key: 'b'}]}
-          renderItem={({item}) => <Text>{item.key}</Text>}
-        />
-        <ToolbarAndroid
-        title="AwesomeApp"
-        actions={[{title: 'Settings', show: 'always'}]}
+          <TabViewAnimated
+          style={styles.container}
+          navigationState={this.state}
+          renderScene={this._renderScene}
+          renderHeader={this._renderHeader}
+          onIndexChange={this._handleIndexChange}
+          initialLayout={initialLayout}
         />
       </View>
     );
@@ -55,10 +131,7 @@ export default class App extends Component<{}> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flex: 1
   },
   welcome: {
     fontSize: 20,
@@ -70,4 +143,25 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+});
+
+
+const cameraStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
 });
